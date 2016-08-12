f0de883772a2302c655e2e7b90ec6856536f87a4
nidhal <nidhalmesselmani@yahoo.fr>
2016-08-12 13:41:29 +0200

    sd

--- config/assets/production.js
+++ config/assets/production.js
@@ -6,6 +6,7 @@ module.exports = {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.min.css',
         'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+        'public/lib/angular-chart.js/dist/angular-chart.css'
       ],
       js: [
         'public/lib/angular/angular.min.js',
@@ -16,7 +17,9 @@ module.exports = {
         'public/lib/angular-ui-utils/ui-utils.min.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
         'public/lib/angular-file-upload/angular-file-upload.min.js',
-        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
+        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/Chart.js/Chart.min.js',
+        'public/lib/angular-chart.js/dist/angular-chart.min.js'
       ]
     },
     css: 'public/dist/application.min.css',
